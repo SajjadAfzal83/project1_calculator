2fd661465aa07e687566783b04e27ea1cc32670d
SajjadAfzal83 <soafzal@gmail.com>
2023-08-11 20:40:39 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# project1_calculator
+ Console interactive Calculator
---
+++ index.js
@@ -0,0 +1,32 @@
+import inquirer from "inquirer";
+let ans = await inquirer.prompt([
+    {
+        type: "number",
+        name: "firstNum",
+        message: "Enter your first number",
+    },
+    {
+        type: "number",
+        name: "secondNum",
+        message: "Enter your second number",
+    },
+    {
+        type: "list",
+        name: "sign",
+        message: "method",
+        choices: ["+", "-", "/", "*"],
+    },
+]);
+if (ans.sign == "+") {
+    console.log(ans.firstNum + ans.secondNum);
+}
+if (ans.sign == "-") {
+    console.log(ans.firstNum - ans.secondNum);
+}
+if (ans.sign == "/") {
+    console.log(ans.firstNum / ans.secondNum);
+}
+if (ans.sign == "*") {
+    console.log(ans.firstNum * ans.secondNum);
+}
+console.log();
